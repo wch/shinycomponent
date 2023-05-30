7b6303b7301e8345b7ed0ab5bb95c02957a5848d
Winston Chang <winston@stdout.org>
2023-05-29 20:52:27 -0500

    Update build script

--- js/scripts/build.ts
+++ js/scripts/build.ts
@@ -1,5 +1,6 @@
 import { build } from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
+import fs from "node:fs";
 
 async function bundle() {
   try {
@@ -7,14 +8,16 @@ async function bundle() {
       entryPoints: ["src/components.ts", "src/datagrid/index.tsx"],
       bundle: true,
       outdir: "out/",
-      // minify: true, // Minify the output
+      minify: true,
       sourcemap: true,
       plugins: [sassPlugin({ type: "css-text" })],
+      metafile: true,
     };
 
     const result = await build(options);
     console.log("Build completed successfully!");
     // console.log("Output:", result);
+    fs.writeFileSync("esbuild-metadata.json", JSON.stringify(result.metafile));
   } catch (error) {
     console.error("Build failed:", error);
   }
