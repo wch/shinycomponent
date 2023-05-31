d5df22d369cddfa0b687603e670837de68312233
Winston Chang <winston@stdout.org>
2023-05-31 11:37:59 -0500

    Add --minify and --metafile build options

--- js/scripts/build.ts
+++ js/scripts/build.ts
@@ -5,6 +5,8 @@ import * as fs from "node:fs";
 // Set a boolean value of watch to true if the flag --watch is provided when the file is run from the command line.
 // E.g. node js/scripts/build.ts --watch
 const watch: boolean = process.argv.includes("--watch");
+const minify: boolean = process.argv.includes("--minify");
+const metafile: boolean = process.argv.includes("--metafile");
 
 async function bundle() {
   try {
@@ -13,15 +15,16 @@ async function bundle() {
         "src/components.ts",
         "src/datagrid/index.tsx",
         "src/m3/index.ts",
+        "src/forge/index.ts",
       ],
       bundle: true,
       outdir: "out/",
-      minify: true,
+      minify: minify,
       sourcemap: true,
       plugins: [
         sassPlugin({ filter: /src\/datagrid\/.*scss$/, type: "css-text" }),
       ],
-      metafile: true,
+      metafile: metafile,
     };
 
     if (watch) {
@@ -34,7 +37,12 @@ async function bundle() {
     const result = await build(options);
     console.log("Build completed successfully!");
     // console.log("Output:", result);
-    fs.writeFileSync("esbuild-metadata.json", JSON.stringify(result.metafile));
+    if (metafile) {
+      fs.writeFileSync(
+        "esbuild-metadata.json",
+        JSON.stringify(result.metafile)
+      );
+    }
   } catch (error) {
     console.error("Build failed:", error);
   }
