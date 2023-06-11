30226d59fb0e6645a63a8e8dd60f78c1fbee88c2
Winston Chang <winston@stdout.org>
2023-06-10 23:08:19 -0500

    Update build script for new version of esbuild

--- js/scripts/build.ts
+++ js/scripts/build.ts
@@ -1,4 +1,4 @@
-import { build, context } from "esbuild";
+import esbuild from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
 import * as fs from "node:fs";
 
@@ -8,44 +8,55 @@ const watch: boolean = process.argv.includes("--watch");
 const minify: boolean = process.argv.includes("--minify");
 const metafile: boolean = process.argv.includes("--metafile");
 
-async function bundle() {
-  try {
-    const options = {
-      entryPoints: {
-        components: "src/components.ts",
-        datagrid: "src/datagrid/index.tsx",
-        m3: "src/m3/index.ts",
-        forge: "src/forge/index.ts",
-      },
-      bundle: true,
-      outdir: "out/",
-      minify: minify,
-      sourcemap: true,
-      plugins: [
-        sassPlugin({ filter: /src\/datagrid\/.*scss$/, type: "css-text" }),
-      ],
-      metafile: metafile,
-    };
+const rebuildLoggerPlugin = {
+  name: "rebuild-logger",
+  setup(build: esbuild.PluginBuild) {
+    build.onStart(() => {
+      console.log(`[${new Date().toISOString()}] Rebuilding JS files...`);
+    });
+  },
+};
 
+const metafilePlugin = {
+  name: "metafile",
+  setup(build: esbuild.PluginBuild) {
+    build.onEnd((result) => {
+      if (metafile) {
+        console.log("metafile");
+        fs.writeFileSync(
+          "esbuild-metadata.json",
+          JSON.stringify(result.metafile)
+        );
+      }
+    });
+  },
+};
+
+esbuild
+  .context({
+    entryPoints: {
+      components: "src/components.ts",
+      datagrid: "src/datagrid/index.tsx",
+      m3: "src/m3/index.ts",
+      forge: "src/forge/index.ts",
+    },
+    bundle: true,
+    outdir: "out/",
+    minify: minify,
+    sourcemap: true,
+    plugins: [
+      rebuildLoggerPlugin,
+      metafilePlugin,
+      sassPlugin({ filter: /src\/datagrid\/.*scss$/, type: "css-text" }),
+    ],
+    metafile: metafile,
+  })
+  .then((context) => {
     if (watch) {
-      console.log("Watching files for changes...");
-      const ctx = await context(options);
-      ctx.watch();
-      return;
+      context.watch();
+    } else {
+      context.rebuild();
+      context.dispose();
     }
-
-    const result = await build(options);
-    console.log("Build completed successfully!");
-    // console.log("Output:", result);
-    if (metafile) {
-      fs.writeFileSync(
-        "esbuild-metadata.json",
-        JSON.stringify(result.metafile)
-      );
-    }
-  } catch (error) {
-    console.error("Build failed:", error);
-  }
-}
-
-bundle();
+  })
+  .catch(() => process.exit(1));
