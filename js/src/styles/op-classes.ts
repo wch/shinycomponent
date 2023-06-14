c9ba6db727484b5cae5fbcb6df5b45d2fcd3456e
Nick Strayer <nick.strayer@gmail.com>
2023-06-14 13:45:16 -0400

    Simplify theme definitions and rename `--surface-shadow` to `--shadow-color` to allow it to be used with open-props built in shadows.

--- js/src/styles/op-classes.ts
+++ js/src/styles/op-classes.ts
@@ -34,16 +34,14 @@ export const themePrimitives = {
     border: 1px solid hsl(var(--brand-hue) 10% 50% / 15%);
     box-shadow: 0 1rem 0.5rem -0.5rem;
     box-shadow: 0 2.8px 2.2px
-        hsl(var(--surface-shadow) / calc(var(--shadow-strength) + 3%)),
-      0 6.7px 5.3px
-        hsl(var(--surface-shadow) / calc(var(--shadow-strength) + 1%)),
-      0 12.5px 10px
-        hsl(var(--surface-shadow) / calc(var(--shadow-strength) + 2%)),
+        hsl(var(--shadow-color) / calc(var(--shadow-strength) + 3%)),
+      0 6.7px 5.3px hsl(var(--shadow-color) / calc(var(--shadow-strength) + 1%)),
+      0 12.5px 10px hsl(var(--shadow-color) / calc(var(--shadow-strength) + 2%)),
       0 22.3px 17.9px
-        hsl(var(--surface-shadow) / calc(var(--shadow-strength) + 2%)),
+        hsl(var(--shadow-color) / calc(var(--shadow-strength) + 2%)),
       0 41.8px 33.4px
-        hsl(var(--surface-shadow) / calc(var(--shadow-strength) + 3%)),
-      0 100px 80px hsl(var(--surface-shadow) / var(--shadow-strength));
+        hsl(var(--shadow-color) / calc(var(--shadow-strength) + 3%)),
+      0 100px 80px hsl(var(--shadow-color) / var(--shadow-strength));
   `,
   /**
    * A series of definitions that will reset text in a component to use standards without having to manually assign everything.
