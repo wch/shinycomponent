cc9f04afe3abbfa9a746892548efe5b432c4bbb6
Nick Strayer <nick.strayer@gmail.com>
2023-06-23 11:22:42 -0400

    Make sidebars a bit smarter about their appearance when not in icon-section mode

--- js/src/sidebar.ts
+++ js/src/sidebar.ts
@@ -1,11 +1,13 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { setElAttr } from "./set_el_attr";
+import { getElementsFromSlotChangeEvent } from "./utils/getElementsFromSlotChangeEvent";
 
 @customElement("shiny-sidebar")
 export class Sidebar extends LitElement {
   @property({ type: Boolean, reflect: true }) closed: boolean = false;
   @property({ type: Number }) openWidthPx: number = 320;
+  @property({ type: Boolean, reflect: true }) collapseToIcons: boolean = false;
 
   // Styles are scoped to this element: they won't conflict with styles
   // on the main page or in other components. Styling API can be exposed
@@ -27,8 +29,9 @@ export class Sidebar extends LitElement {
       --open-content-overflow: auto;
       --closed-content-overflow: hidden;
       --closed-content-padding: 0;
+      --closed-content-opacity: 0;
       --open-width: var(--sidebar-width, 30vw);
-      --closed-width: calc(4 * var(--padding));
+      --closed-width: var(--padding);
 
       /* Default settings */
       --transition: var(--speed-fast) var(--ease-3);
@@ -36,12 +39,14 @@ export class Sidebar extends LitElement {
       --sidebar-content-columns: var(--open-content-columns);
       --sidebar-content-height: var(--open-content-height);
       --sidebar-content-gap: var(--open-content-gap);
+      --content-opacity: 1;
 
       height: 100%;
       position: relative;
       display: flex;
       flex-direction: column;
       gap: var(--padding);
+      background-color: var(--surface-2);
     }
 
     @container (max-width: 700px) {
@@ -65,6 +70,7 @@ export class Sidebar extends LitElement {
       --sidebar-content-height: var(--closed-content-height);
       --sidebar-content-gap: var(--closed-content-gap);
       --sidebar-content-overflow: var(--closed-content-overflow);
+      --content-opacity: var(--closed-content-opacity);
 
       width: var(--closed-width);
       cursor: e-resize;
@@ -79,7 +85,7 @@ export class Sidebar extends LitElement {
       transition: width var(--transition), padding var(--transition);
       display: flex;
       flex-direction: column;
-      background-color: var(--surface-2);
+      background-color: inherit;
     }
 
     :host([closed]) .content {
@@ -102,10 +108,6 @@ export class Sidebar extends LitElement {
       text-align: center;
     }
 
-    :host([closed]) .toggle-icon {
-      transform: scaleX(0);
-    }
-
     :host([open]) .toggle-icon {
       transition: transform var(--transition);
       transform: scaleX(1);
@@ -113,14 +115,31 @@ export class Sidebar extends LitElement {
 
     .open-toggle {
       position: absolute;
-      top: 0;
-      right: 0;
+      top: var(--size-xxs);
+      left: 0;
       font-size: var(--font-size-h4);
       font-weight: var(--font-weight-headings);
       width: var(--padding);
       height: fit-content;
       cursor: pointer;
       color: var(--text-2);
+    }
+
+    :host([closed]) .toggle-icon {
+      rotate: 180deg;
+    }
+
+    :host([closed][collapseToIcons]) .content {
+      width: var(--padding);
+    }
+
+    :host([closed]:not([collapseToIcons])) .content {
+      opacity: 0;
+    }
+
+    :host([closed][collapseToIcons]),
+    :host([closed][collapseToIcons]) .content {
+      width: calc(4 * var(--padding));
     }
   `;
 
@@ -129,9 +148,7 @@ export class Sidebar extends LitElement {
     setElAttr(this, "slot", "sidebar");
 
     this.addEventListener("click", (e) => {
-      if (!this.closed) {
-        return;
-      }
+      if (!this.closed) return;
 
       this.toggleClosed();
     });
@@ -167,17 +184,29 @@ export class Sidebar extends LitElement {
     this.autoCollapseOnSmallScreens();
   }
 
+  handleSlottedElementsChange(e: Event) {
+    const elementsInSlot = getElementsFromSlotChangeEvent(e);
+
+    // If we exclusivly have <shiny-section> elements in the sidebar then we should enable the icon collapse mode
+    const hasOnlySections = elementsInSlot.every(
+      (el) => el.tagName === "SHINY-SECTION"
+    );
+
+    this.collapseToIcons = hasOnlySections;
+    console.log("hasOnlySections", hasOnlySections);
+  }
+
   render() {
     return html`
       <div class="content" style="--sidebar-width: ${this.openWidthPx}px;">
-        <slot></slot>
-        <div
-          @click=${this.handleToggleBtnClick}
-          title=${this.closed ? "Open sidebar" : "Close sidebar"}
-          class="open-toggle"
-        >
-          <div class="toggle-icon">❮</div>
-        </div>
+        <slot @slotchange=${this.handleSlottedElementsChange}></slot>
+      </div>
+      <div
+        @click=${this.handleToggleBtnClick}
+        title=${this.closed ? "Open sidebar" : "Close sidebar"}
+        class="open-toggle"
+      >
+        <div class="toggle-icon">❮</div>
       </div>
     `;
   }
