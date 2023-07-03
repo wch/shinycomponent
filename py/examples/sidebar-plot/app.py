0dd3c7b94dc2fcfa9ff4f09e948505fcd26d7a8d
Winston Chang <winston@stdout.org>
2023-07-03 17:40:54 -0500

    Add sidebar plot app

---
+++ py/examples/sidebar-plot/app.py
@@ -0,0 +1,32 @@
+# pyright: basic
+
+import matplotlib.pyplot as plt
+import numpy as np
+from shiny import App, render, ui
+
+import shinycomponent as sc
+
+app_ui = sc.page_dashboard(
+    sc.sidebar(
+        sc.forge.input_slider("n", "N", 0, 100, 20),
+    ),
+    sc.grid(
+        sc.grid_item(
+            ui.output_plot("histogram"),
+            width=2,
+            height=1,
+        )
+    ),
+)
+
+
+def server(input, output, session):
+    @output
+    @render.plot(alt="A histogram")
+    def histogram():
+        np.random.seed(19680801)
+        x = 100 + 15 * np.random.randn(437)
+        plt.hist(x, input.n(), density=True)
+
+
+app = App(app_ui, server, debug=True)
