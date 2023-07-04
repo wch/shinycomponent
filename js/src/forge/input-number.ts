c2a953723b362538ec99bd1dbc1d6ef15f5253c4
Winston Chang <winston@stdout.org>
2023-07-03 22:00:05 -0500

    Fix debounce behavior for input_number

--- js/src/forge/input-number.ts
+++ js/src/forge/input-number.ts
@@ -1,3 +1,5 @@
+import SlInput from "@shoelace-style/shoelace/dist/components/input/input.js";
+import debounce from "just-debounce-it";
 import { CSSResultGroup, css } from "lit";
 import { property } from "lit/decorators.js";
 import { makeValueChangeEmitter } from "../make_value_change_emitter";
@@ -5,50 +7,88 @@ import {
   CustomElementInputGetValue,
   makeInputBinding,
 } from "../shiny/make-input-binding";
-import { ForgeInputText } from "./input-text";
 
 export class ForgeInputNumber
-  extends ForgeInputText
+  extends SlInput
   implements CustomElementInputGetValue<number>
 {
-  onValueChange = makeValueChangeEmitter(this, this.id);
-
-  @property({ type: Number }) min: number = -Infinity;
-  @property({ type: Number }) max: number = Infinity;
-  @property({ type: Boolean }) invalid: boolean = false;
-
   static styles: CSSResultGroup = [
+    SlInput.styles,
     css`
+      .form-control--has-label.form-control--medium .form-control__label {
+        margin-bottom: var(--size-xs);
+        font-size: var(--font-size-m);
+      }
+
       div.invalid {
         outline-offset: -1px;
         outline: 3px solid var(--danger);
       }
     `,
-    ForgeInputText.styles,
   ];
+
+  @property({ type: Number }) min: number = -Infinity;
+  @property({ type: Number }) max: number = Infinity;
+  @property({ type: Boolean }) invalid: boolean = false;
+  @property({ type: Boolean, attribute: "wait-for-enter" })
+  waitForEnter: boolean = false;
+  @property({ type: Number }) debounce: number = 200;
+
+  onValueChange = makeValueChangeEmitter(this, this.id);
+  onChangeCallback: (x: boolean) => void = (x: boolean) => {};
+
+  // Record the most recent valid value. We do this because of debouncing: if
+  // the user types a valid value, and then keeps adding digits to make it
+  // invalid, we want to send the last valid value (before it went into the
+  // invalid range).
+  //
+  // For example, if starting value is 1 and the max is 1000, then if the user
+  // types 2, 2, 2, in quick succession, the values in the box will be 12, 122,
+  // and 1222. We want to send 122, but this requires a bit of record keeping
+  // because updates are debounced. That's what lastValidValue is for.
+  lastValidValue: number;
 
   constructor() {
     super();
+    this.type = "number";
+    this.lastValidValue = Number(this.value);
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    const notifyChangeDebounced = debounce(
+      () => this.notifyChange(),
+      this.debounce
+    );
+
     this.addEventListener("input", () => {
+      if (this.waitForEnter) return;
+
       const rawValue = this.shadowRoot!.querySelector("input")!.valueAsNumber;
 
       if (rawValue >= this.min && rawValue <= this.max) {
-        this.value = String(rawValue);
+        this.lastValidValue = rawValue;
         this.invalid = false;
+        notifyChangeDebounced();
       } else {
         this.invalid = true;
       }
       this.updateInvalidClass();
     });
-    this.type = "number";
+
+    this.addEventListener("keydown", (e) => {
+      if (!this.waitForEnter) return;
+      if (e.code === "Enter") {
+        this.notifyChange();
+      }
+    });
+
+    this.addEventListener("blur", (e) => this.notifyChange());
   }
 
   getValue(): number {
-    return Number(this.value);
-  }
-
-  connectedCallback(): void {
-    super.connectedCallback();
+    return this.lastValidValue;
   }
 
   updateInvalidClass() {
@@ -61,9 +101,6 @@ export class ForgeInputNumber
   }
 
   notifyChange(): void {
-    if (this.invalid) {
-      return;
-    }
     this.onChangeCallback(true);
     this.onValueChange({ type: "number", value: this.getValue() });
   }
