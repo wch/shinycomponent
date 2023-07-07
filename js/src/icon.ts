376c87b409c7c615c5aecf314273544d54bcea53
Nick Strayer <nick.strayer@rstudio.com>
2023-07-07 13:48:16 -0400

    Add jsdoc to icon component

--- js/src/icon.ts
+++ js/src/icon.ts
@@ -2,12 +2,26 @@ import "iconify-icon";
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
-// For list of all available icons, see:
-// https://icon-sets.iconify.design/
+/**
+ * A custom element that displays an icon using the Iconify library. For list of
+ * all available icons, see: https://icon-sets.iconify.design/
+ * @customElement shiny-icon
+ * @extends LitElement
+ */
 @customElement("shiny-icon")
 export class ShinyIcon extends LitElement {
+  /**
+   * The name of the icon to display using iconify format.
+   * @property {string} name
+   * @default "info-circle"
+   */
   @property() name: string = "info-circle";
 
+  /**
+   * The styles for the ShinyIcon element.
+   * @static
+   * @property {CSSResult} styles
+   */
   static styles = css`
     :host {
       display: inline-block;
@@ -15,6 +29,10 @@ export class ShinyIcon extends LitElement {
     }
   `;
 
+  /**
+   * Renders the ShinyIcon element.
+   * @returns {TemplateResult} The rendered template for the ShinyIcon element.
+   */
   render() {
     return html`<iconify-icon icon="${this.name}"></iconify-icon>`;
   }
