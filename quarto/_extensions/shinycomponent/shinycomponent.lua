54eacf0bc3cb9c435e70f7d15d8a4187a11af328
Nick Strayer <nick.strayer@rstudio.com>
2023-07-11 18:10:27 -0400

    Fix up example to pull in assets correctly and also to utilize the arguments

--- quarto/_extensions/shinycomponent/shinycomponent.lua
+++ quarto/_extensions/shinycomponent/shinycomponent.lua
@@ -2,16 +2,22 @@ local function ensureHtmlDeps()
   quarto.doc.add_html_dependency({
     name = 'shinycomponent',
     version = '0.1.0',
-    stylesheets = {'assets/components.js', 'assets/open-props.min.css', 'assets/shiny-theme.css'}
+    scripts = {'assets/components.js'},
+    stylesheets = {'assets/open-props.min.css', 'assets/shiny-theme.css'}
   })
 end
 
 return {
   ['shinycomponent'] = function(args, kwargs, meta)
     ensureHtmlDeps()
-    return pandoc.RawBlock(
-      "html",
-      "<value-box bg='purple' title='quarto value box' value='42'> </value-box>"
-  )
+
+    return pandoc.Blocks({
+      pandoc.RawBlock("html","<value-box bg='purple'>"),
+      pandoc.Div(kwargs.title, {slot = "title"}),
+      pandoc.Div(kwargs.value, {slot = "value"}),
+      pandoc.Div(kwargs.subvalue,{slot = "subvalue"}),
+      pandoc.RawBlock("html", "</value-box>")
+    })
+
   end
 }
