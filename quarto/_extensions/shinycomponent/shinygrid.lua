c2e7a3658d5a90562b4280282eed4dbb675f934e
Nick Strayer <nick.strayer@rstudio.com>
2023-07-12 14:58:11 -0400

    Add a template for dashboard layouts, beginings of python htmldependency management example of shiny-backend

--- quarto/_extensions/shinycomponent/shinygrid.lua
+++ quarto/_extensions/shinycomponent/shinygrid.lua
@@ -1,16 +1,67 @@
+local function ensureHtmlDeps()
+  quarto.doc.add_html_dependency({
+    name = 'shinycomponent',
+    version = '0.1.0',
+    scripts = {{path='assets/components.js'}},
+    stylesheets = {'assets/open-props.min.css', 'assets/shiny-theme.css'}
+  })
+end
+
 
 return {
-  Div=function(el)
-    if el.classes:includes("grid") then
-      return pandoc.Div(
-        pandoc.Blocks({
-          pandoc.Div("I am a grid"),
-          pandoc.Div(el.content),
-        }),
-        pandoc.Attr(el.identifier, el.classes)
-      )
+  {
+    -- This needs to be top level nested so we know it runs before the other
+    -- filters and thus lets us know that we're in dashboard mode as set by the
+    -- template
+    Meta = function(meta)
+      ensureHtmlDeps()
+    end,
+
+    Pandoc = function(doc)
+
+      local blocks = pandoc.List()
+      doc.blocks = pandoc.structure.make_sections(doc.blocks)
+
+
+      quarto.utils.dump(doc.blocks)
+      return doc
     end
-  end
+  },
+  {
+    RawBlock= function(el)
+      print("Raw block content", el.text)
+      if el.format == "html" and el.text:match("<forge%-") then
+        print("Found dark mode switch")
+        quarto.doc.add_html_dependency({
+          name = 'shinycomponent-forge',
+          version = '0.1.0',
+          scripts = {{path='assets/forge.js'}}
+        })
+      end
+    end,
+    Div=function(el)
+      if el.classes:includes("sc-grid") then
+
+        ensureHtmlDeps()
+
+        -- Convert attributes into string with format: "key1=value1;key2=value2"
+        local openTag = "<shiny-grid "
+        for k, v in pairs(el.attributes) do
+          openTag = openTag .. k .. "=" .. v .. " "
+        end
+
+        openTag = openTag .. ">"
+
+        -- Make a shiny-grid opening tag with attributes
+        local children = el.content
+        children:insert(1, pandoc.RawBlock("html", openTag))
+        children:insert(pandoc.RawBlock("html", "</shiny-grid>"))
+
+        return el
+      end
+
+    end
+  }
 }
 
 
