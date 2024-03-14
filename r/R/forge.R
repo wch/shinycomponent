1e934269ab4d2b9200a36e46ddd9d8f9fba698b6
Winston Chang <winston@posit.co>
2024-03-14 11:47:08 -0500

    Update R/quarto example

--- r/R/forge.R
+++ r/R/forge.R
@@ -1,9 +1,13 @@
 #' @importFrom htmltools tag htmlDependency tagList tags
 
 #' @export
-input_slider <- function(id, ...) {
+input_slider <- function(id, label = NULL, ...) {
   tagList(
     forge_dep(),
-    tag("forge-input-slider", list(id = id, ...))
+    tag("forge-input-slider", list(
+      id = id,
+      if (!is.null(label)) div(label, slot = "label"),
+      ...
+    ))
   )
 }
